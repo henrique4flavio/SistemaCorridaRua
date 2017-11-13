7eb11d2214c0a0cb7529c2a6698448d6107cb448
LaisAlves <laisalvesjf@hotmail.com>
2017-11-13 19:13:29 -0200

    Upgrade

--- SCCR/src/java/controller/PesquisaLoteController.java
+++ SCCR/src/java/controller/PesquisaLoteController.java
@@ -3,6 +3,9 @@ package controller;
 
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
@@ -35,15 +38,6 @@ public class PesquisaLoteController extends HttpServlet {
                    }
     }
 
-    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
-    /**
-     * Handles the HTTP <code>GET</code> method.
-     *
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
-     */
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
