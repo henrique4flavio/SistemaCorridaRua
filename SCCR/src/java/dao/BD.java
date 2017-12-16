42e7ff20cd0088a19a8297b07bb545eee16706c9
Jonathas Araújo <31988572+Jonathasaraujo@users.noreply.github.com>
2017-12-16 18:08:24 -0200

    UP

--- SCCR/src/java/dao/BD.java
+++ SCCR/src/java/dao/BD.java
@@ -9,7 +9,7 @@ public class BD {
     public static Connection getConexao() throws ClassNotFoundException, SQLException {
         Connection conexao = null;
         Class.forName("com.mysql.jdbc.Driver");
-        conexao = DriverManager.getConnection("jdbc:mysql://localhost/sccr", "root", "1234");
+        conexao = DriverManager.getConnection("jdbc:mysql://localhost/sccr", "root", "");
         return conexao;
     }
 }
