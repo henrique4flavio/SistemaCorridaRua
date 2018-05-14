0da6d0d03baede7523bd73b20d9ff54e00b13b76
Jonathas Araújo <31988572+Jonathasaraujo@users.noreply.github.com>
2018-05-14 14:51:21 -0300

    up

---
+++ SCCR/src/main/java/com/fjl/SCCR/dao/AtletaDAO.java
@@ -0,0 +1,7 @@
+package com.kmv.goforrest.repository;
+
+import com.kmv.goforrest.model.Atleta;
+import org.springframework.data.repository.CrudRepository;
+
+public interface AtletaDAO extends CrudRepository<Atleta, Long> {
+}
