ed6ea45d95bd6877cfa8f7de087fc760b87f6a1f
henrique4flavio <flaviohenriquejf@gmail.com>
2018-05-23 20:45:08 -0300

    upgrade

---
+++ corrida/src/main/java/com/corridasjf/corrida/repository/AdministradorRepository.java
@@ -0,0 +1,7 @@
+package com.corridasjf.corrida.repository;
+
+import com.corridasjf.corrida.model.Administrador;
+import org.springframework.data.repository.CrudRepository;
+
+public interface AdministradorRepository extends CrudRepository<Administrador, Long> {
+}
---
+++ corrida/src/main/java/com/corridasjf/corrida/repository/AtletaRepository.java
@@ -0,0 +1,7 @@
+package com.corridasjf.corrida.repository;
+
+import com.corridasjf.corrida.model.Atleta;
+import org.springframework.data.repository.CrudRepository;
+
+public interface AtletaRepository extends CrudRepository<Atleta, Long> {
+}
