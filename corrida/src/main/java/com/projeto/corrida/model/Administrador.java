45854236e79bc380e986c4065bb0827a207decb8
Jonathas Araújo <31988572+Jonathasaraujo@users.noreply.github.com>
2018-06-12 08:25:00 -0300

    Funcionando com herança

--- corrida/src/main/java/com/projeto/corrida/model/Administrador.java
+++ corrida/src/main/java/com/projeto/corrida/model/Administrador.java
@@ -6,46 +6,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import java.io.Serializable;
 
-@Entity
 @Data
-public class Administrador {
+@Entity
+public class Administrador extends Usuario {
 
-    @Id
-    @GeneratedValue
-    private Long id;
-
-    private String login;
-    private String senha;
-
-    @Autowired
-    public Administrador(Long id, String login, String senha) {
-        this.id = id;
-        this.login = login;
-        this.senha = senha;
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getLogin() {
-        return login;
-    }
-
-    public void setLogin(String login) {
-        this.login = login;
-    }
-
-    public String getSenha() {
-        return senha;
-    }
-
-    public void setSenha(String senha) {
-        this.senha = senha;
+    public Administrador(Long id, String nome, String cpf, String senha, String email, String celular) {
+        super(id, nome, cpf, senha, email, celular);
     }
 }
