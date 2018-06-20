cf150ce3383ca9d409785815f4d1e0344b7e78fb
Flávio Henrique de Almeida Junior <flaviohenriquejf@gmail.com>
2018-06-20 20:32:03 -0300

    upgrade

--- corrida/src/main/java/com/projeto/corrida/controller/PercursoController.java
+++ corrida/src/main/java/com/projeto/corrida/controller/PercursoController.java
@@ -16,56 +16,54 @@ public class PercursoController {
 
     @Autowired
     private PercursoRepository percursoRepository;
-
     @Autowired
-    private ProvaRepository provaRepository;
-
+    private ProvaRepository  provaRepository;
 
     @GetMapping(value = "listar")
     public String listaPercurso(Model model) {
         model.addAttribute("percursos", percursoRepository.findAll());
         model.addAttribute("operacao", "listar");
         model.addAttribute("title", "Lista de percursos");
-        model.addAttribute("botaoOperacao", "Adicionar Percurso");
+        model.addAttribute("botaoOperacao", "Adicionar percurso");
 
         return "percurso/pesquisaPercurso";
     }
 
     @GetMapping(value = "add")
-    public String displayPercursoForm(Model model) {
-        model.addAttribute("title", "Adicionar prova");
+    public String displayCorredorForm(Model model) {
+        model.addAttribute("title", "Adicionar percurso");
         model.addAttribute("operacao", "adicionar");
         model.addAttribute("provas", provaRepository.findAll());
-        model.addAttribute("botaoOperacao", "Cadastrar percurso");
+        model.addAttribute("botaoOperacao", "Adicionar percurso");
         return "percurso/manterPercurso";
     }
 
     @PostMapping(value = "add")
-    public String processProvaForm(@ModelAttribute Percurso percurso) {
+    public String processKitForm(@ModelAttribute Percurso percurso) {
         percursoRepository.save(percurso);
-        return "redirect:/percurso/listar";
+        return "redirect:/percurso/listar"; // url para qual página quero voltar.
     }
 
-    @GetMapping(value = "edit/{id}")
+    @GetMapping(value = "edit/{id}") // site.com/corredor/edit
     public String percursoEdit(Model model, @PathVariable Long id) {
         Optional<Percurso> percurso = percursoRepository.findById(id);
         model.addAttribute("operacao", "editar");
-        model.addAttribute("botaoOperacao", "Editar percurso");
-        model.addAttribute("title", "Editar percurso");
+        model.addAttribute("botaoOperacao", "Editar Kit");
+        model.addAttribute("title", "Editar Kit");
         model.addAttribute("provas", provaRepository.findAll());
 
         model.addAttribute("percursos",percursoRepository.findAll());
 
-        if (percurso.isPresent()) {
+        if (percurso.isPresent()){
             model.addAttribute("percurso", percurso.get());
         }
-        model.addAttribute("title", "Editar Percurso");
+        model.addAttribute("title", "Editar percurso");
         return "percurso/manterPercurso";
     }
 
-    @PostMapping(value = "edit/{id}")
+    @PostMapping(value = "edit/{id}") // site.com/corredor/edit/1/
     public String edit(@ModelAttribute Percurso percurso, Model model,
-                           @PathVariable Long id) throws Exception {
+                       @PathVariable Long id) throws Exception {
         if (id.equals(percurso.getId())) {
             percursoRepository.save(percurso);
         } else {
@@ -74,25 +72,25 @@ public class PercursoController {
         return "redirect:/percurso/listar";
     }
 
-    @GetMapping(value = "delete/{id}")
-    public String percursoDelete(Model model, @PathVariable Long id) {
+    @GetMapping(value = "delete/{id}") // site.com/corredor/delete/1
+    public String administradorDelete(Model model, @PathVariable Long id) {
         Optional<Percurso> percurso = percursoRepository.findById(id);
-
         model.addAttribute("provas", provaRepository.findAll());
+
         model.addAttribute("operacao", "deletar");
         model.addAttribute("botaoOperacao", "Excluir percurso");
         model.addAttribute("title", "Excluir percurso");
-
         if (percurso.isPresent()) {
             model.addAttribute("percurso", percurso.get());
         }
-        model.addAttribute("title", "Excluir percurso");
+        model.addAttribute("tittle", "Excluir percurso");
         return "percurso/manterPercurso";
     }
 
-    @PostMapping(value = "delete/{id}")
+    @PostMapping(value = "delete/{id}") // site.com/corredor/delete/1
     public String delete(@PathVariable Long id, @ModelAttribute Percurso percurso) {
         percursoRepository.delete(percurso);
         return "redirect:/percurso/listar";
     }
+
 }
