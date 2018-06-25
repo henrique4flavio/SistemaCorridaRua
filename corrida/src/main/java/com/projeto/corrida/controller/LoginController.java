de81a8e9d5ed92ef9ac0ad5aa2041cf907f92f81
Jonathas Araújo <31988572+Jonathasaraujo@users.noreply.github.com>
2018-06-25 13:22:39 -0300

    up logout

--- corrida/src/main/java/com/projeto/corrida/controller/LoginController.java
+++ corrida/src/main/java/com/projeto/corrida/controller/LoginController.java
@@ -326,10 +326,100 @@ public class LoginController extends HttpServlet {
     }
 
     @GetMapping(value = "/logout")
-    public String getLogout(Model model, HttpSession session) {
+    public String getLogout(Model model) {
         model.addAttribute("msg", "");
-        session.invalidate();
-        return "redirect:/";
+        HttpSession session = new HttpSession() {
+            @Override
+            public long getCreationTime() {
+                return 0;
+            }
+
+            @Override
+            public String getId() {
+                return null;
+            }
+
+            @Override
+            public long getLastAccessedTime() {
+                return 0;
+            }
+
+            @Override
+            public ServletContext getServletContext() {
+                return null;
+            }
+
+            @Override
+            public void setMaxInactiveInterval(int i) {
+
+            }
+
+            @Override
+            public int getMaxInactiveInterval() {
+                return 0;
+            }
+
+            @Override
+            public HttpSessionContext getSessionContext() {
+                return null;
+            }
+
+            @Override
+            public Object getAttribute(String s) {
+                return null;
+            }
+
+            @Override
+            public Object getValue(String s) {
+                return null;
+            }
+
+            @Override
+            public Enumeration<String> getAttributeNames() {
+                return null;
+            }
+
+            @Override
+            public String[] getValueNames() {
+                return new String[0];
+            }
+
+            @Override
+            public void setAttribute(String s, Object o) {
+
+            }
+
+            @Override
+            public void putValue(String s, Object o) {
+
+            }
+
+            @Override
+            public void removeAttribute(String s) {
+
+            }
+
+            @Override
+            public void removeValue(String s) {
+
+            }
+
+            @Override
+            public void invalidate() {
+
+            }
+
+            @Override
+            public boolean isNew() {
+                return false;
+            }
+        };
+
+        if (session != null) {
+            session.invalidate();
+
+        }
+        return "login";
     }
 
 
